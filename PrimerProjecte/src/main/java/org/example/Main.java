8e033add5bd0943b805a575beb4131d566e880bd
vladislavgoncear <vladislavgoncear@iesebre.com>
2023-09-19 13:15:08 +0200

    He afegit el primer projecte Java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Vaig a mostrar per la pantalla el misatge "Hello World"
+        //Shortcut del System.out.println() --> sout
+        System.out.println("Hello World!");
+        System.out.println("Segona linia de text");
+        System.out.println(" Me dic Vlad");
+    }
+}
